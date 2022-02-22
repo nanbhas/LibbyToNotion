b63a324026905a26ec4afc580a7ebe27b33c133f
Nandita Bhaskhar <nanbhas@stanford.edu>
2022-02-22 08:09:47 -0800

    update secret file paths in constants

--- globalStore/constants.py
+++ globalStore/constants.py
@@ -3,4 +3,5 @@ Author: Nandita Bhaskhar
 All global constants
 """
 
-### All global constants here
+NOTION_SECRET_FILE = "../secrets/secrets_notion.json"
+LIBBY_SECRET_FILE = "../secrets/secrets_libby.json"
